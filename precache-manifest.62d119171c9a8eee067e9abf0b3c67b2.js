6f6a4d2d004ccf5380287a3223c678f7a379907c
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-03-20 18:05:22 +0200

    Updates

---
+++ precache-manifest.62d119171c9a8eee067e9abf0b3c67b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12184f69dd7bcd024ee1841093190ac3",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "b66c8a41a64094e57556",
+    "url": "/goit-react-hw-04-movies/static/css/main.4a61a5a5.chunk.css"
+  },
+  {
+    "revision": "ce6d40e1eedef6dad424",
+    "url": "/goit-react-hw-04-movies/static/js/2.516c308a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/goit-react-hw-04-movies/static/js/2.516c308a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66c8a41a64094e57556",
+    "url": "/goit-react-hw-04-movies/static/js/main.0e2ba402.chunk.js"
+  },
+  {
+    "revision": "e34e4fd2a07b25362320",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.aa12acaf.js"
+  }
+]);
